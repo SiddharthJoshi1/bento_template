3bd0f5498f5cb54162c87529114d9c6b5849d884
Siddharth Joshi <siddharthjoshi1@hotmail.com>
2026-07-01 20:05:54 +0100

    feat: add scroll benchmark utility (Ctrl+B in debug mode)
    Programmatic scroll benchmark that measures actual frame build+paint
    time using addPostFrameCallback. Prints avg/p50/p95/p99/max stats and
    flags frames exceeding 16ms. Only active in debug builds.

--- lib/presentation/widgets/bento_grid/bento_sliver_list.dart
+++ lib/presentation/widgets/bento_grid/bento_sliver_list.dart
@@ -1,28 +1,58 @@
 import 'package:bento_template/domain/entities/profile_data.dart';
 import 'package:bento_layout/bento_layout.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../../core/constants.dart';
 import '../../../../domain/entities/tile_config.dart';
 import '../../../core/responsive/breakpoints.dart';
 import '../../../core/responsive/mobile_tile_adapter.dart';
 import '../../extensions/tile_size_extension.dart';
+import '../../utils/benchmark/scroll_benchmark.dart';
 import '../profile/profile_section.dart';
 import 'tiles/smart_bento_tile.dart';
 
-class BentoSliverList extends StatelessWidget {
+class BentoSliverList extends StatefulWidget {
   final List<TileConfig> tiles;
   final ProfileData profileData;
 
   const BentoSliverList({super.key, required this.tiles, required this.profileData});
 
   @override
+  State<BentoSliverList> createState() => _BentoSliverListState();
+}
+
+class _BentoSliverListState extends State<BentoSliverList> {
+  final ScrollController _scrollController = ScrollController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  KeyEventResult _onKey(FocusNode node, KeyEvent event) {
+    if (!kDebugMode) return KeyEventResult.ignored;
+    if (event is KeyDownEvent &&
+        event.logicalKey == LogicalKeyboardKey.keyB &&
+        HardwareKeyboard.instance.isControlPressed) {
+      ScrollBenchmark.run(_scrollController);
+      return KeyEventResult.handled;
+    }
+    return KeyEventResult.ignored;
+  }
+
+  @override
   Widget build(BuildContext context) {
     final bool isMobile = Breakpoints.isMobile(context);
 
     final List<TileConfig> resolvedTiles = isMobile
-        ? tiles.map((t) => MobileTileAdapter.getMobileConfig(t)).toList()
-        : tiles;
+        ? widget.tiles.map((t) => MobileTileAdapter.getMobileConfig(t)).toList()
+        : widget.tiles;
 
     final List<BentoItem> items = resolvedTiles
         .map(
@@ -40,7 +70,11 @@ class BentoSliverList extends StatelessWidget {
 
     final horizontalPadding = isMobile ? 0.0 : 20.0;
 
-    return Center(
+    return Focus(
+      focusNode: _focusNode,
+      autofocus: true,
+      onKeyEvent: _onKey,
+      child: Center(
       child: LayoutBuilder(
         builder: (context, constraints) {
           final availableWidth = constraints.maxWidth - (horizontalPadding * 2);
@@ -52,6 +86,7 @@ class BentoSliverList extends StatelessWidget {
           );
 
           return SingleChildScrollView(
+            controller: _scrollController,
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
               child: Column(
@@ -59,7 +94,7 @@ class BentoSliverList extends StatelessWidget {
                   const SizedBox(height: 20),
 
                   if (isMobile) ...[
-                    ProfileSection(profile: profileData),
+                    ProfileSection(profile: widget.profileData),
                     const SizedBox(height: 20),
                   ],
 
@@ -85,6 +120,7 @@ class BentoSliverList extends StatelessWidget {
           );
         },
       ),
+    ),
     );
   }
 }
